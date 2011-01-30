a31b25e929c9109d723f9842f621357ba75fd51d
Ilya <instenet@gmail.com>
2011-01-30 22:26:45 +0300

    Working on qsort

--- qsort.erl
+++ qsort.erl
@@ -2,31 +2,7 @@
 -export([qsort/1]).
 
 
-% Splits the list into two
-
-divide([]) -> {[],[]};
-divide([Head]) -> {Head, []};
-divide([Head | Tail]) -> lists:split(length([Head | Tail]) div 2, [Head | Tail]).
-
-% Interleaving the lists
-
-interleave([], []) -> [],
-interleave(X, []) -> X,
-interleave([], Y) -> Y,
-interleave([LH | LT], [RH | RT]) ->
-    if
-	LH >= RH -> LeftList = [RH | interleave(LT, RT)], RightList = [LH | interleave()],
-	LH < RH -> LeftList = [LH | LT], RightList = [RH | RT]
-    end,
-    RR = lists:reverse(RightList),
-    
-
-% Sorting
-
-qsort([]) -> [],
-qsort([X]) -> [X],
-qsort([Head | Tail]) ->
-    {Left, Right} = divide([Head | Tail]),
-    %RightWithoutTail = lists:delete(length(Right), Right),
-    ReverseRight = lists:reverse(Right),
-    
+qsort([]) ->
+     [];
+qsort([H | T]) ->
+    qsort([ S || S <- T, S < H]) ++ [H] ++ qsort([ B || B <- T, B >= H]).
